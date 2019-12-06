1c91e7b44856a47377d9488991a452a0e58d9566
Oliver Okoth. Otieno <ookoth@hq.ekenya.co.ke>
2019-12-06 11:11:50 +0300

    ädiing more changes to my local repo

--- DatabaseSeeder.php
+++ DatabaseSeeder.php
@@ -8,9 +8,18 @@ class DatabaseSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+	 *Oliver made the changes
      */
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+		
+		if var abs==null
+		echo='i am a bad person'
+		 
+		 else 
+			 echo='get_browser settings'
+		 
+		 end
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git_DevOps_Test
 The is a first project for devOps training using GIT
+
+More changes coming
